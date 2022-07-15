e5663872ccbe567aaeaeae67f20a95d8d232ad4c
Andrea Agnolin <agnophi@gmail.com>
2022-07-15 20:45:07 +0200

    aggiunta hello world iniziale

---
+++ code/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="box">
+        <section>
+          Ciao Mondo
+        </section>
+        <section class="has-text-right">
+          <button class="button">
+            Button
+          </button>
+        </section>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
