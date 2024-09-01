0e5daed6342b4f534c405ffd376a135cdcfce774
Andrea Agnolin <agnophi@gmail.com>
2024-09-01 21:56:25 +0200

    aggiornato vue e dipendenze

--- code/src/main.js
+++ code/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+// import router from './router'
 
 import '@/assets/main.scss'
 
-const app = createApp(App).use(store)
+const app = createApp(App)
+
+app.use(store)
+// app.use(router)
 
 import './include/fa'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
